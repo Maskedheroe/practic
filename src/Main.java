0012ec5cc506b1b427fa100a01f5b166f61ea1d2
877406889 <877406889@qq.com>
2018-11-04 15:57:38 +0800

    add

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
 
+    /*
+    * 1 git init
+    * 2 git add .
+    * 3 git commit -m""
+    * 4 git push origin master(branch name)
+    * */
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
