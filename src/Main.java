0b1b8e0a313166d986b8240b25b7a22c953ea52d
877406889 <877406889@qq.com>
2018-11-04 15:53:19 +0800

    first

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        show();
+
+    }
+
+    private static void show() {
+        String []str = new String[]{};
+        for (String s : str) {
+        }
+    }
+}
